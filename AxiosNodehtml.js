05c1368d0cd155913ba3ebc0fae93428256dec61
Piya0073 <s6406021612031@email.kmutnb.ac.th>
2025-02-18 08:58:46 +0700

    aa

--- AxiosNodehtml.js
+++ AxiosNodehtml.js
@@ -5,7 +5,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
-const path = requre("path");
+const path = require("path");
 // Base URL for the API
 // const base_url = "https://api.example.com";
 const base_url = "http://localhost:5000";
